95621d9c55de65d705555c73ee3b2b311cd962c7
Josef Jezek <josef@jezek.co.cz>
2013-07-31 17:33:36 +0200

    Fix Slider and Contact form, Add link to forum

--- userconf.py
+++ userconf.py
@@ -11,19 +11,29 @@ DISQUS_SITENAME = 'openstackcz'
 TWITTER_USERNAME = 'openstackcz'
 FACEBOOK_APPID = ''
 GOOGLE_SITE_SEARCH_URL = 'http://localhost:8000'
-CONTACT_MAP_LOCATION = 'Praha,+Czech'
+CONTACT_MAP_LOCATION = u'Praha,+Czech'
 
 # Contact form
 GOOGLE_CONTACT_FORM_URL = 'https://docs.google.com/a/jezek.co.cz/forms/d/15yaSGXK4yZGm3D3jeH2_tEm5PE7fhA65OmeezkBjpaM/formResponse'
-GOOGLE_CONTACT_FORM_NAME = 'entry.352057519'
-GOOGLE_CONTACT_FORM_EMAIL = 'entry.162652585'
-GOOGLE_CONTACT_FORM_SUBJECT = 'entry.423243377'
-GOOGLE_CONTACT_FORM_MESSAGE = 'entry.534812453'
+GOOGLE_CONTACT_FORM_NAME_ID = 'entry.352057519'
+GOOGLE_CONTACT_FORM_EMAIL_ID = 'entry.162652585'
+GOOGLE_CONTACT_FORM_SUBJECT_ID = 'entry.423243377'
+GOOGLE_CONTACT_FORM_MESSAGE_ID = 'entry.534812453'
+
+# Links in top menu
+#MENUITEMS = (
+#    (u'', ''),
+#)
+
+# External links in top menu
+MENUITEMS_EXTERNAL = (
+    (u'Fórum', 'https://groups.google.com/forum/#!forum/openstackcz'),
+)
 
 # Blogroll
 LINKS = (
-    ('OpenStack Blog', 'http://www.openstack.org/blog/'),
-    ('OpenStack Planet', 'http://planet.openstack.org/'),
+    (u'OpenStack Blog', 'http://www.openstack.org/blog/'),
+    (u'OpenStack Planet', 'http://planet.openstack.org/'),
 )
 
 # Social widget
@@ -33,5 +43,5 @@ SOCIAL = (
 )
 
 # SEO
-META_KEYWORDS = 'openstack, cloud, linux, ubuntu, red hat'
+META_KEYWORDS = u'openstack, cloud, linux, ubuntu, red hat'
 META_DESCRIPTION = u'Česká (a slovenská) komunita kolem svobodné technologie OpenStack pro provoz cloudů typu IaaS (infrastruktura jako služba).'
